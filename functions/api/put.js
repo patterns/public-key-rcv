b3d055f319a3ab2f5b00acdcdf89939a8d8ab8d5
興怡 <>
2023-03-26 14:03:04 -0700

    ttl

--- functions/api/put.js
+++ functions/api/put.js
@@ -3,16 +3,18 @@
  */
 export async function onRequestPost(context) {
   try {
+    //TODO accept json data
     let input = await context.request.formData();
     const loc = input.get('locator');
     if (!loc) {
-      return new Response('input missing locator field', { status: 400 });
+      return new Response('Missing locator input field', { status: 400 });
     }
 
-    const internal_seq = crypto.randomUUID();
+    // use the SHA256 sum as our internal sequence
+    const internal_seq = crypto.subtle.digest({name: 'SHA-256'}, loc);
     const KV = context.env.VERIFIER_KEYS;
-    await KV.put(internal_seq, loc);
-    let output = { detail: "put completed" };
+    await KV.put(internal_seq, loc, {expirationTtl: 3600});
+    let output = { detail: "store done" };
     let pretty = JSON.stringify(output, null, 2);
     return new Response(pretty, {
       headers: {
