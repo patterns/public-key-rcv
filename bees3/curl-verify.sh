742ba56fa7bd00b853bc9fa63944f35610476d13
興怡 <>
2023-09-30 13:06:50 -0700

    base64 external

--- bees3/curl-verify.sh
+++ bees3/curl-verify.sh
@@ -3,8 +3,8 @@
 # TODO (base64 is safe as json data?)
 # 1. the signature arrives as base64 and needs to be decoded into u8[256] for pkcs1-verify
 #    which means leave it as base64 in the json data value
-# 2. the hashed must be calc'd as the digest of the base input text
-#    which means encode into base64 to be the json data value (?)
+# 2. the hashed is the digest of the base input text and already hexadecimal
+
 
 curl -X POST -H "content-type: application/json" --verbose \
      --data @curl-data.json \
